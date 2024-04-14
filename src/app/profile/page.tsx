09d9a71af482aba169e7d997e2c3d6e45dd30d14
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-04-14 22:08:44 +0400

    protect profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,22 @@
-const PrifilePage = () => {
-    return ( 
-        <div className="profile">This is profile page</div>
-     );
-}
- 
-export default PrifilePage;
+import { redirect } from "next/navigation";
+import { getSession } from "../actions";
+
+const ProfilePage = async () => {
+  const session = await getSession();
+
+  if(!session.isLoggedIn){
+    redirect("/")
+  }
+
+  return (
+    <div className="profile">
+      <h1>Welcome to the ProfilePage</h1>
+      <p>
+        Welcome, <b>{session.username}</b>
+      </p>
+      <span>You are a <b>{session.isPro ? "Premium" : "Free"}</b> user</span>
+    </div>
+  );
+};
+
+export default ProfilePage;
