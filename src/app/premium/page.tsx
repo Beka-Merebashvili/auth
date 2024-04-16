7071b88b880a70380ce673f2fbbb40a6225fd21d
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-04-16 15:23:46 +0400

    protect premium page

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -1,7 +1,35 @@
-const PremiumPage = () => {
-    return ( 
-        <div className="premium">This is Premium Page</div>
-     );
-}
- 
-export default PremiumPage;
+import { redirect } from "next/navigation";
+import { getSession } from "../actions";
+import Link from "next/link";
+
+const PremiumPage = async () => {
+    const session = await getSession();
+  
+    if (!session.isLoggedIn) {
+      redirect("/");
+    }
+  
+    if (!session.isPro) {
+      return (
+        <div className="notPremium">
+          <h1>Only premium users can see the content!</h1>
+          <Link href="/profile">
+            Go to the profile page to upgrade to premium
+          </Link>
+        </div>
+      );
+    }
+  
+    return (
+      <div className="premium">
+        <h1>Welcome to the PremiumPage</h1>
+        <ul>
+          <li>Movies</li>
+          <li>Series</li>
+          <li>Podcasts</li>
+        </ul>
+      </div>
+    );
+  };
+  
+  export default PremiumPage;
